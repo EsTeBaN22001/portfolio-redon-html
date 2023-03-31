9be7c67ffc75e10d5adf1942fe979e07f7e9ede5
Esteban Redón <esteban1.redon2@gmail.com>
2023-03-31 19:50:22 -0300

    Implementación del nuevo diseño del sitio web

--- src/js/app.js
+++ src/js/app.js
@@ -6,47 +6,6 @@ if(document.querySelector('.header')){
     header.classList.toggle('down', window.scrollY > 0);
   })
 
-  const menuIcon = document.querySelector('.menu-icon');
-
-  menuIcon.addEventListener('click', function(){
-
-    const nav = document.querySelector('.nav');
-
-    nav.classList.toggle('active');
-
-  })
-
-}
-
-// Código para señalar como activo los enlaces de la página en el footer
-if(document.querySelector('.footer')){
-
-  const body = document.querySelector('body');
-
-  // Página del index
-  if(body.classList.contains('index')){
-    const linkIndexFooter = document.querySelector('.navFooter a[href="/"]');
-    linkIndexFooter.classList.add('activeLink');
-  }
-
-  // Página del about-us
-  if(body.classList.contains('about-us')){
-    const linkIndexFooter = document.querySelector('.navFooter a[href="/about-us"]');
-    linkIndexFooter.classList.add('activeLink');
-  }
-
-  // Página del about-us
-  if(body.classList.contains('contact')){
-    const linkIndexFooter = document.querySelector('.navFooter a[href="/contact"]');
-    linkIndexFooter.classList.add('activeLink');
-  }
-
-  // Página del about-us
-  if(body.classList.contains('proyects')){
-    const linkIndexFooter = document.querySelector('.navFooter a[href="/proyects"]');
-    linkIndexFooter.classList.add('activeLink');
-  }
-
 }
 
 // Código para el pre-loader de la página
@@ -55,31 +14,3 @@ window.addEventListener('load', function(){
   const loader = document.querySelector('.loader');
   loader.classList.toggle('loader2');
 })
-
-
-// Código para el read-more de los proyectos
-
-if(document.querySelector('.proyects-cards-container')){
-
-  const proyectsCardsContainer = document.querySelector('.proyects-cards-container')
-
-  proyectsCardsContainer.addEventListener('click', function(e){
-
-    if(e.target.classList.value == 'read-more'){
-
-      const readMoreButton = e.target
-
-      const extendedText = e.target.previousElementSibling
-      extendedText.classList.toggle('read-more-active')
-
-      if(extendedText.classList.contains('read-more-active')){
-        readMoreButton.textContent = "Leer menos..."
-      }else{
-        readMoreButton.textContent = "Leer más..."
-      }
-
-    }
-
-  })
-
-}
--- src/js/customScrollReveal-index.js
+++ src/js/customScrollReveal-index.js
@@ -2,73 +2,34 @@
 window.sr = ScrollReveal();
 
 const distance = '25px';
-const duration = 800;
+const duration = 500;
 
-sr.reveal('.reveal-left', {
-  duration: duration,
-  origin: 'left',
-  distance: `${distance}`,
-})
+// Scroll reveal options
 
-sr.reveal('.reveal-right', {
-  duration: duration,
-  origin: 'right',
-  distance: `${distance}`,
-})
-
-sr.reveal('.reveal-top', {
-  duration: duration,
-  distance: `${distance}`,
-  origin: 'top'
-});
-
-sr.reveal('.reveal-bottom', {
-  duration: duration,
-  distance: `6px`,
-  origin: 'bottom',
-  delay: 700
-})
-
-// Scroll reveal VERTICAL
-
-sr.reveal('.reveal-vertical1',{
-  duration: duration,
-  distance: `${distance}`,
-  origin: 'top'
-})
-
-sr.reveal('.reveal-vertical2',{
+sr.reveal('.reveal1',{
   duration: duration,
   distance: `${distance}`,
   origin: 'bottom',
   delay: 300
 })
 
-sr.reveal('.reveal-vertical3',{
+sr.reveal('.reveal2',{
   duration: duration,
   distance: `${distance}`,
-  origin: 'top',
+  origin: 'bottom',
   delay: 600
 })
 
-// Scroll reveal HORIZONTAL
-
-sr.reveal('.reveal-horizontal1',{
+sr.reveal('.reveal3',{
   duration: duration,
   distance: `${distance}`,
-  origin: 'left'
+  origin: 'bottom',
+  delay: 900
 })
 
-sr.reveal('.reveal-horizontal2',{
+sr.reveal('.reveal4',{
   duration: duration,
   distance: `${distance}`,
-  origin: 'right',
-  delay: 300
-})
-
-sr.reveal('.reveal-horizontal3',{
-  duration: duration,
-  distance: `${distance}`,
-  origin: 'left',
-  delay: 600
+  origin: 'bottom',
+  delay: 1200
 })
