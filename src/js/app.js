723cd29c28158891ea062ea3ae3789e80070c38b
Esteban <esteban1.redon2@gmail.com>
2025-04-24 00:36:31 -0300

    Rediseño de la web

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,14 @@
 // Efecto del navBar al hacer scroll
-if(document.querySelector('.header')){
-
-  const header = document.querySelector('.header');
-  window.addEventListener('scroll', function(){
-    header.classList.toggle('down', window.scrollY > 0);
+if (document.querySelector('.header')) {
+  const header = document.querySelector('.header')
+  window.addEventListener('scroll', function () {
+    header.classList.toggle('down', window.scrollY > 0)
   })
-
 }
 
 // Código para el pre-loader de la página
 
-window.addEventListener('load', function(){
-  const loader = document.querySelector('.loader');
-  loader.classList.toggle('loader2');
+window.addEventListener('load', function () {
+  const loader = document.querySelector('.loader')
+  loader.classList.toggle('loader2')
 })
